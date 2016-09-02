8a710b3fc0bc3802d9514a516469051512dadfbb
eskab <szymon.kabelis@yahoo.com>
2016-09-02 14:16:38 +0200

    Todo app ready

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 >## My first React App
+
+todo: organize code, read about good practices 
--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-const DATA = [
-  {id: 0, description: 'In React, inline styles are not specified as a string. Instead they are specified with an object whose key'},
-  {id: 1, description: 'Access current weather data for any location on Earth including over 200,000 cities!'},
-  {id: 2, description: 'The next thing you’ll need to do is add this data to the App components as its props.'},
-  {id: 3, description: 'Below, you’ll see how you pass the data into the App component, by simply changing a little bit on the ReactDOM.render method.'},
-  {id: 4, description: 'I recently started working with ReactJS. Specifically, I am utilizing the react-rails ruby gem and react-bootstrap components.'},
-  {id: 5, description: 'When fetching data asynchronously, use componentWillUnmount to cancel any outstanding requests before the component is unmounted.'},
-  {id: 6, description: 'IT to z pewnością rynek pracownika, co oznacza, że ma on często duży wpływ na kształtowanie warunków umowy o pracę.'},
-  {id: 7, description: 'Re: transpiling with Babel, its caveats page says that Array.from is required for spread, but at present '}
+let DATA = [
+  {id: 0, description: 'In React, inline styles are not specified as a string. Instead they are specified with an object whose key', done: false},
+  {id: 1, description: 'Access current weather data for any location on Earth including over 200,000 cities!', done: false},
+  {id: 2, description: 'The next thing you’ll need to do is add this data to the App components as its props.', done: false},
+  {id: 3, description: 'Below, you’ll see how you pass the data into the App component, by simply changing a little bit on the ReactDOM.render method.', done: false},
+  {id: 4, description: 'I recently started working with ReactJS. Specifically, I am utilizing the react-rails ruby gem and react-bootstrap components.', done: false},
+  {id: 5, description: 'When fetching data asynchronously, use componentWillUnmount to cancel any outstanding requests before the component is unmounted.', done: false},
+  {id: 6, description: 'IT to z pewnością rynek pracownika, co oznacza, że ma on często duży wpływ na kształtowanie warunków umowy o pracę.', done: false},
+  {id: 7, description: 'Re: transpiling with Babel, its caveats page says that Array.from is required for spread, but at present ', done: false}
 ];
 
 const TaskListStyles = {
@@ -22,26 +22,41 @@ const TaskStyles = {
   color: '#555',
   padding: '10px',
   margin: '5px 0',
-  fontSize: '11px',
-  backgroundColor: '#eee',
-  textTransform: 'uppercase',
-  letterSpacing: '1px'
+  fontSize: '14px',
+  backgroundColor: '#f5f5f5'
 }
 
-const Task = React.createClass({
+const NewTaskForm = React.createClass({
   render: function() {
     return (
-      <li style={TaskStyles}><span>{this.props.description}</span></li>
+      <form className="new-task-form">
+        <input name="newTask" type="text" />
+        <button onClick={this.props.add}>Add me</button>
+      </form>
+    );
+  }
+});
+
+const Task = React.createClass({
+  render() {
+    return (
+      <li style={TaskStyles} data-id={this.props.id} data-done={this.props.done} className={this.props.done}>
+        <span>{this.props.description}</span>
+        <button onClick={this.props.del}>X</button>
+        <button onClick={this.props.toggle}>Mark</button>
+      </li>
     );
   }
 });
 
 const TaskList = React.createClass({
   render: function() {
+    let props = this.props;
+
     return (
       <ul style={TaskListStyles} className="task-container">
         {this.props.data.map(function(result) {
-          return <Task key={result.id} description={result.description} />;
+          return <Task key={result.id} id={result.id} description={result.description} done={result.done} del={props.del} toggle={props.toggle} />;
         })}
       </ul>
     );
@@ -49,11 +64,51 @@ const TaskList = React.createClass({
 });
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = { data: DATA };  
+    this.add = this.add.bind(this);
+    this.del = this.del.bind(this);
+    this.toggle = this.toggle.bind(this);
+  }
+  
+  add(e) {
+    e.preventDefault();
+    let task = document.getElementsByName('newTask');
+
+    DATA.push({id: DATA.length, description: task[0].value});
+    this.setState({data: DATA});    
+  }
+
+  toggle(e) {
+    e.preventDefault();
+
+    DATA[e.target.parentNode.dataset.id].done = !DATA[e.target.parentNode.dataset.id].done;
+    this.setState({ data: DATA });
+  }  
+  
+  del(e) {
+    e.preventDefault();
+
+    DATA.splice(e.target.parentNode.dataset.id, 1);
+    this.sortIds(() => {
+      this.setState({data: DATA});
+    });
+  }
+
+  sortIds(cb) {
+    for (let i = 0; i < DATA.length; i++) {
+      DATA[i].id = i;
+    }
+    cb();
+  }
+
   render() {
     return (
       <div className="my-first-react-app">
         <h1>List of tasks</h1>
-        <TaskList data={DATA} />
+        <NewTaskForm add={this.add} />
+        <TaskList data={DATA} del={this.del} toggle={this.toggle} />
       </div>
     );
   }
