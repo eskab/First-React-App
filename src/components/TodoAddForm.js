4380348b05d51dafe0deda219ec6a7d84844ed07
eskab <szymon.kabelis@yahoo.com>
2016-09-07 11:35:13 +0200

    TodoAdd Component

--- src/components/TodoAddForm.js
+++ src/components/TodoAddForm.js
@@ -1,15 +1,23 @@
 import React, { PropTypes } from 'react';
 
-const handleAddTodo = (e, id, cb) => {
-  console.log(e);
+const handleAddTodo = (e, id, cb, input) => {
+  if (e.type === 'keypress' && e.key !== 'Enter' || !input.value) {
+    return;
+  }
+
+  cb(id, input.value);
 };
 
-const TodoAddForm = ({ todos, handler }) => (
-  <div className="todo-add-new">
-    <input onKeyPress={(e) => handleAddTodo(e, todos.length, handler)} />
-    <button onClick={(e) => handleAddTodo(e, todos.length, handler)}>Add</button>
-  </div>
-);
+const TodoAddForm = ({ todos, handler }) => {
+  let input;
+
+  return (
+    <div className="todo-add-new">
+      <input onKeyPress={(e) => handleAddTodo(e, todos.length, handler, input)} ref={node => { input = node; }} />
+      <button onClick={(e) => handleAddTodo(e, todos.length, handler, input)}>Add</button>
+    </div>
+  )
+};
 
 TodoAddForm.propTypes = {
   handler: PropTypes.func.isRequired
