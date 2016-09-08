64e7cc3e01cda6331d0f48fbe8faf003397bb186
eskab <szymon.kabelis@yahoo.com>
2016-09-08 08:51:10 +0200

    Deleted old code

--- src/components/TodoAddForm.js
+++ src/components/TodoAddForm.js
@@ -24,36 +24,3 @@ TodoAddForm.propTypes = {
 };
 
 export default TodoAddForm;
-
-// import React, { Component } from 'react';
-
-// export default class TodoAdd extends Component {
-//   constructor(props) {
-//     super();
-//     this.props = props;
-//     this.handleAddAction = this.handleAddAction.bind(this);
-//   }
-
-//   handleAddAction(e) {
-//     if (e.type === 'keypress' && e.key !== 'Enter' || !this.refs.addNewTodo.value) {
-//       return;
-//     }
-
-//     this.props.store.dispatch({
-//       type: 'ADD_TODO',
-//       text: this.refs.addNewTodo.value,
-//       id: this.props.store.getState().todos.length,
-//       completed: false
-//     });
-//     this.refs.addNewTodo.value = '';
-//   }
-
-//   render() {
-//     return (
-//       <div className="todo-add-new">
-//         <input onKeyPress={this.handleAddAction} ref="addNewTodo" />
-//         <button onClick={this.handleAddAction}>Add</button>
-//       </div>
-//     );
-//   }
-// };
